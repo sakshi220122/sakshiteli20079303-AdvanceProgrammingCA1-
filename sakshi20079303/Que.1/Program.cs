ef1d4db0f3883d8ea578d0494bba191ad753dae1
sakshi220122 <sakshii.teli2004@gmail.com>
2025-11-30 17:56:20 +0000

    Add files via upload

---
+++ sakshi20079303/Que.1/Program.cs
@@ -0,0 +1,201 @@
+﻿#nullable disable
+
+
+using System;
+
+class MainClass
+{
+    static ContactBook[] contacts = new ContactBook[100];
+    static int count = 0;
+
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            ContactBook CB = new ContactBook();
+            CB.FirstName = "Person" + (i + 1);
+            CB.LastName = "lastname" + (i + 1);
+            CB.Company = "company" + (i + 1);
+            CB.Mobile = "123456789" + (i + 1);
+            CB.Email = "person" + (i + 1) + "@gmail.com";
+            CB.BirthDate = "1990-01-01";
+            contacts[count] = CB;
+            count++;
+        }
+       
+
+
+        while (true)
+        {
+            Console.WriteLine("\n--- Contact Book Menu --- ");
+            Console.WriteLine(" 1. Add contact ");
+            Console.WriteLine(" 2. Show All Contacts ");
+            Console.WriteLine(" 3. Show one Contacts ");
+            Console.WriteLine(" 4. Update Contact ");
+            Console.WriteLine(" 5. Delete Contact ");
+            Console.WriteLine(" 0. Exit ");
+            Console.WriteLine(" Enter Option ");
+
+            string str = Console.ReadLine();
+
+            switch (str)
+            {
+                case "0":
+                    return;
+
+                case "1":
+                    AddContact();
+                    break;
+
+                case "2":
+                    ShowAll();
+                    break;
+
+                case "3":
+                    ShowOne();
+                    break;
+
+                case "4":
+                    UpdateContact();
+                    break;
+
+                case "5":
+                    DeleteContact();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+    }   
+
+    static void AddContact()
+    {
+        ContactBook CB = new ContactBook();
+
+        Console.Write("First Name :");
+        CB.FirstName = Console.ReadLine();
+
+        Console.Write("Last Name :");
+        CB.LastName = Console.ReadLine();
+
+        Console.Write("Company : ");
+        CB.Company = Console.ReadLine();
+
+        string mobileNumber = "";
+
+        while (true)
+        {
+            Console.Write("Mobile Number ( 9 digits ) : ");
+            mobileNumber = Console.ReadLine();
+
+            if (mobileNumber.Length == 9 && mobileNumber != "000000000" && long.TryParse(mobileNumber, out _))
+                break;
+
+            Console.WriteLine("Invalid Mobile Number . Please Try Again...");
+        }
+
+        CB.Mobile = mobileNumber;
+
+        Console.Write("Email : ");
+        CB.Email = Console.ReadLine();
+
+        Console.Write("Birthdate :");
+        CB.BirthDate = Console.ReadLine();
+
+        contacts[count] = CB;
+        count++;
+
+        Console.WriteLine("Contact Added !");
+    }
+
+    static void ShowAll()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(contacts[i].FirstName + " " + contacts[i].LastName + " -- " + contacts[i].Mobile);
+        }
+    }
+
+    static void ShowOne()
+    {
+        Console.Write("Enter Mobile Number :");
+        string mobileNumber = Console.ReadLine();
+
+        for (int i = 0; i < count; i++)
+        {
+            if (contacts[i].Mobile == mobileNumber)
+            {
+                contacts[i].showContact("Contact Details ");  
+                return;
+            }
+        }
+
+        Console.WriteLine("Contact not found....");   
+    }
+
+    static void UpdateContact()
+    {
+        Console.Write("Enter Mobile Number To Update : ");
+        string mobileNumber = Console.ReadLine();
+
+        for (int i = 0; i < count; i++)
+        {
+            if (contacts[i].Mobile == mobileNumber)
+            {
+                Console.Write("New First Name :");
+                string FN = Console.ReadLine();
+                if (FN != "") contacts[i].FirstName = FN;
+
+                Console.Write("New Last Name :");
+                string LN = Console.ReadLine();
+                if (LN != "") contacts[i].LastName = LN;
+
+                Console.Write("New Company :");
+                string COM = Console.ReadLine();
+                if (COM != "") contacts[i].Company = COM;
+
+                Console.Write("New Email:");
+                string EM = Console.ReadLine();
+                if (EM != "") contacts[i].Email = EM;
+
+                Console.Write("New Birthdate  :");
+                string BD = Console.ReadLine() ;
+                if (BD != "") contacts[i].BirthDate = BD;
+
+                Console.WriteLine("Contact Updated ! ");
+                return;
+            }
+        }
+
+        Console.WriteLine("Contact not found....");   
+    }
+
+    static void DeleteContact()
+    {
+        Console.Write(" Enter Mobile Number To Delete : ");
+        string mobileNumber = Console.ReadLine();
+
+        for (int i = 0; i < count; i++)
+        {
+            if (contacts[i].Mobile == mobileNumber)
+            {
+                for (int j = i; j < count - 1; j++)
+                {
+                    contacts[j] = contacts[j + 1];
+                }
+                count--;
+                Console.WriteLine(" Contact Deleted ! ");
+                return;
+            }
+        }
+
+        Console.WriteLine("Contact not found....");
+    }
+}
+
+
+
+//To run program we have to first go in cd C:\Users\Admin\Desktop\sakshi20079303\Que.1
+//Then inside this folder run - dotnet run
---
+++ sakshi20079303/Que.2/ConsoleApp1/Program.cs
@@ -0,0 +1,197 @@
+﻿#nullable disable
+using System;
+
+
+class MainClass
+{
+    static FileExtension[] extension = new FileExtension[200];
+    static int count = 0;
+
+    public static void Main(string[] args)
+    {
+         AddDefaultExtension();
+
+         while(true)
+        {
+            Console.WriteLine("\n---File Extension ---");
+            Console.WriteLine(" 1.Show All Extensions ");
+            Console.WriteLine(" 2.Show One Extensions ");
+            Console.WriteLine(" 3.Add Extensions ");
+            Console.WriteLine(" 4.Update Extensions ");
+            Console.WriteLine(" 5.Delete Extensions ");
+            Console.WriteLine(" 0.Exit");
+            Console.Write("Enter option: ");
+
+            string str = Console.ReadLine();
+
+            switch(str)
+            {
+               
+            
+                case "0":
+                    return;
+                case "1":
+                    ShowAll();
+                    break;
+                case "2":
+                    Search();
+                    break;
+                case "3":
+                    AddExtension();
+                    break;
+                case "4":
+                    UpdateExtension();
+                    break;
+                case "5":
+                    DeleteExtension();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, try again....");
+                    break;
+            }
+
+            
+
+        }
+
+    }
+
+    static void AddDefaultExtension()
+    {
+       Add(".mp4",  "Video File");
+        Add(".avi",  "Video File");
+        Add(".mov",  "Video File");
+        Add(".mkv",  "Video File");
+        Add(".webm", "Video File");
+        Add(".mp3",  "Audio File");
+        Add(".wav",  "Audio File");
+        Add(".flac", "Audio File");
+        Add(".jpg",  "Image File");
+        Add(".jpeg", "Image File");
+        Add(".png",  "Image File");
+        Add(".gif",  "Image File");
+        Add(".pdf",  "Document");
+        Add(".docx", "Word Document");
+        Add(".xlsx", "Excel Sheet");
+        Add(".pptx", "PowerPoint");
+        Add(".txt",  "Text File");
+        Add(".zip",  "Compressed File");
+        Add(".rar",  "Compressed File");
+        Add(".7z",   "Compressed File");
+    
+         
+    }
+
+    static void Add(string ext, string desc)
+    {
+        FileExtension FE = new FileExtension();
+        FE.Extension = ext;
+        FE.Description = desc;
+        extension[count++] = FE;
+
+    }
+
+    static void ShowAll()
+    {
+        Console.WriteLine("\n Supported Extensions : ");
+        for(int i = 0 ; i < count ; i++)
+        {
+            Console.WriteLine(extension[i].Extension + " - " + extension[i].Description);
+        }
+    }
+
+    static void Search()
+    {
+        Console.Write(" Enter Extension : ");
+        string ext = Console.ReadLine();
+
+        if (ext.Length > 0 && ext[0] != '.')
+            ext = "." + ext;
+
+        for(int i=0;i<count;i++)
+        {
+            if(extension[i].Extension == ext)
+            {
+                extension[i].showExtension();
+                return;
+            }
+        } 
+
+        Console.WriteLine(" Extension Not Found ....");   
+
+    }
+
+    static void AddExtension()
+    {
+        Console.Write("Enter Extension : ");
+        string ext = Console.ReadLine();
+        if (ext != "" && ext[0] != '.')
+            ext = "." + ext;
+
+        Console.Write(" Enter Description : ");
+        string desc = Console.ReadLine();
+
+        Add(ext,desc);
+        Console.WriteLine("Entension added ! ");
+
+    }
+
+    static void UpdateExtension()
+    {
+        Console.Write("Extension : ");
+        string ext = Console.ReadLine();
+
+        if (ext != "" && ext[0] != '.')
+            ext = "." + ext;
+
+
+        for ( int i = 0; i < count; i++)
+        {
+            if(extension[i].Extension == ext)
+            {
+                Console.Write("New Description : ");
+                string D = Console.ReadLine();
+                if(D != "") extension[i].Description = D;
+
+                Console.WriteLine(" Extension Updated ");
+                return;
+            }
+        }   
+
+        Console.WriteLine(" Extension Not Found ");
+
+    }
+
+    static void DeleteExtension()
+    {
+        Console.Write("Extension : ");
+        string ext = Console.ReadLine();
+
+        if(ext != "" && ext[0] != '.')
+           ext = "." + ext;
+
+        for(int i = 0; i< count;i++)
+        {
+            
+            if(extension[i].Extension == ext)
+            {
+                for(int j = i ; j<count-1; j++)
+                {
+                    extension[j] = extension[j+1];
+                }
+
+                count--;
+                Console.WriteLine(" Extension Deleted :");
+                return;
+            }
+        }   
+
+        Console.WriteLine("Extension Not Found ....");
+    }
+
+}
+
+
+
+//1. cd "C:\Users\Admin\Desktop\sakshi20079303\Que.2\ConsoleApp1" 
+//2.dotnet run
